2e3a534dec7031f6f9a708e8bfb877efdfb2f255
UmerAR <79077500+UmerAR@users.noreply.github.com>
2025-04-13 22:59:45 +0400

    some changes

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -71,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_AT,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
         KC_DLR,  KC_HASH, KC_EXLM, KC_LPRN, KC_RPRN, KC_MINS,                               KC_PLUS, KC_LBRC, KC_RBRC, KC_AMPR, KC_PIPE, KC_TILD,
         XXXXXXX, KC_CIRC, KC_PERC, KC_LABK, KC_RABK, KC_QUES,                               KC_EQL,  KC_LCBR, KC_RCBR, KC_BSLS, KC_ASTR, XXXXXXX,
-                                            KC_TAB,  KC_SPC, XXXXXXX,              MO(NAV), KC_ENT,  KC_BSPC
+                                            KC_TAB,  KC_SPC, XXXXXXX,              XXXXXXX, KC_ENT,  KC_BSPC
     ),
     
      /*
       * ┌────┬────┬────┬────┬────┬────┐       ┌────┬────┬────┬────┬────┬────┐
       * │    │ F1 │ F2 │ F3 │ F4 │ F5 │       │ F6 │ F7 │ F8 │ F9 │ F10│    │
       * ├────┼────┼────┼────┼────┼────┤       ├────┼────┼────┼────┼────┼────┤
-      * │    │ F11│ F12│Mute│ V- │ V+ │       │ ←  │ ↓  │ ↑  │ →  │Home│ End│
+      * │    │ F11│ F12│    │    │    │       │ ←  │ ↓  │ ↑  │ →  │Home│ End│
       * ├────┼────┼────┼────┼────┼────┤       ├────┼────┼────┼────┼────┼────┤
-      * │    │    │    │    │PSCR│    │       │    │PgDn│PgUp│ Del│    │    │
+      * │    │    │    │PSCR│Mute│ V- │       │ V+ │PgDn│PgUp│ Del│    │    │
       * └────┴────┴────┴────┴────┴────┘       └────┴────┴────┴────┴────┴────┘   
       *               ┌───┐                   ┌───┐
       *               │Tab├───┐           ┌───┤Bsp│
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [NAV] = LAYOUT_split_3x6_3(
         XXXXXXX, KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  XXXXXXX,
-        XXXXXXX, KC_F11,  KC_F12, KC_MUTE, KC_VOLD, KC_VOLU,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_END,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX,                            XXXXXXX, KC_PGDN, KC_PGUP, KC_DEL,   XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_END,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_MUTE, KC_VOLD,                          KC_VOLU, KC_PGDN, KC_PGUP, KC_DEL,   XXXXXXX, XXXXXXX,
                                             KC_TAB,  KC_SPC, XXXXXXX,           XXXXXXX, KC_ENT,  KC_BSPC  
     )
 };
